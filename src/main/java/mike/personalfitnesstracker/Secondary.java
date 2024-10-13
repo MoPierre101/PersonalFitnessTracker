49c358f032c1efd2b44e32da430deea6a2bf3392
molew-creator <molew679@gmail.com>
2024-10-13 15:09:09 -0400

    Created basic login and sign up screen

---
+++ src/main/java/mike/personalfitnesstracker/Secondary.java
@@ -0,0 +1,54 @@
+package mike.personalfitnesstracker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+public class Secondary
+{
+    @FXML
+    private TextField heightTF;
+    @FXML
+    private TextField nameTF;
+    @FXML
+    private RadioButton newcomerRB;
+    @FXML
+    private RadioButton proRB;
+    @FXML
+    private CheckBox RippedCB;
+    @FXML
+    private RadioButton amateurRB;
+    @FXML
+    private CheckBox loseWeightCB;
+    @FXML
+    private CheckBox gainMuscleCB;
+    @FXML
+    private TextField emailTF;
+    @FXML
+    private TextField ageTF;
+    @FXML
+    private TextField targetWeightTF;
+    @FXML
+    private TextField currentWeightTF;
+    @FXML
+    private TextField userNameTF;
+    @FXML
+    private TextField passwordTF;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+
+    }
+
+    @FXML
+    public void getLevel(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void getGoals(ActionEvent actionEvent) {
+
+    }
+}
