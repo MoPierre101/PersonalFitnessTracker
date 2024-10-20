b09a8053f4e9546a9c4bb784b77add4f2765f0ce
molew-creator <molew679@gmail.com>
2024-10-20 15:29:03 -0400

    Added transitions between graphs window and home window

--- src/main/java/mike/personalfitnesstracker/ChartsController.java
+++ src/main/java/mike/personalfitnesstracker/ChartsController.java
@@ -1,12 +1,15 @@
 package mike.personalfitnesstracker;
 
 import javafx.event.ActionEvent;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Date;
 
 public class ChartsController {
@@ -16,12 +19,54 @@ public class ChartsController {
 
     @javafx.fxml.FXML
     public void initialize() {
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Date");
 
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Weight");
+
+        LineChart lineChart = new LineChart(xAxis, yAxis);
+
+        XYChart.Series data = new XYChart.Series();
+        data.setName("Weight Overtime");
+
+        //provide data
+        data.getData().add(new XYChart.Data("10/17/24", 134.0));
+        data.getData().add(new XYChart.Data("10/18/24", 138.0));
+        data.getData().add(new XYChart.Data("10/19/24", 142.0));
+        data.getData().add(new XYChart.Data("10/20/24", 146.0));
+
+        lineChart.getData().add(data);
+        lineChart.setLegendVisible(false);
+
+        //add barChart to borderPane
+        borderPane.setCenter(lineChart);
     }
 
     @javafx.fxml.FXML
-    public void handleShowPieChart(ActionEvent actionEvent) {
+    public void handleShowLineChart(ActionEvent actionEvent) {
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Date");
 
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Weight");
+
+        LineChart lineChart = new LineChart(xAxis, yAxis);
+
+        XYChart.Series data = new XYChart.Series();
+        data.setName("Weight Overtime");
+
+        //provide data
+        data.getData().add(new XYChart.Data("10/17/24", 134.0));
+        data.getData().add(new XYChart.Data("10/18/24", 138.0));
+        data.getData().add(new XYChart.Data("10/19/24", 142.0));
+        data.getData().add(new XYChart.Data("10/20/24", 146.0));
+
+        lineChart.getData().add(data);
+        lineChart.setLegendVisible(false);
+
+        //add barChart to borderPane
+        borderPane.setCenter(lineChart);
     }
 
     @javafx.fxml.FXML
@@ -51,12 +96,19 @@ public class ChartsController {
     }
 
     @javafx.fxml.FXML
-    public void handleClose(ActionEvent actionEvent) {
-        System.exit(0);
+    public void handleClose(ActionEvent event) throws IOException {
+        Parent homeParent = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene homeScene = new Scene(homeParent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(homeScene);
+        window.centerOnScreen();
+        window.show();
     }
 
     @javafx.fxml.FXML
     public void handleUpdateData(ActionEvent actionEvent) {
-
+        
     }
 }
