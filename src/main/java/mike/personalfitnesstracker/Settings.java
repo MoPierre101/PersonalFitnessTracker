c5af4be1ff7778d0a1b806d07cd79841897df05a
krispierre <pierkl@farmingdale.edu>
2024-10-24 12:21:00 -0400

    fix setting window

--- src/main/java/mike/personalfitnesstracker/Settings.java
+++ src/main/java/mike/personalfitnesstracker/Settings.java
@@ -9,14 +9,14 @@ import javafx.scene.control.ToggleGroup;
 
 public class Settings {
 
-    public ToggleGroup themeToggleGroup;
-    public Button returnButton;
+    @FXML
+    private ToggleGroup themeToggleGroup;  // Make sure to annotate this with @FXML
+    @FXML
+    private Button returnButton;
     @FXML
     private ComboBox<String> fontSizeComboBox;
-
     @FXML
     private RadioButton lightModeRadioButton;
-
     @FXML
     private RadioButton darkModeRadioButton;
 
@@ -28,37 +28,31 @@ public class Settings {
 
     @FXML
     private void handleReturnAction() {
-        // Logic to return to the previous screen
         System.out.println("Returning to the previous screen...");
         // Implement navigation logic here
     }
 
     @FXML
     private void handleUpdateProfilePicture() {
-        // Logic to update the profile picture
         System.out.println("Updating profile picture...");
         // Implement profile picture update logic here
     }
 
     @FXML
     private void handleChangeUsername() {
-        // Logic to change the username
         System.out.println("Changing username...");
         // Implement username change logic here
     }
 
     @FXML
     private void handleLightMode() {
-        // Logic to switch to light mode
         System.out.println("Switched to Light Mode");
         // Implement theme change logic here
     }
 
     @FXML
     private void handleDarkMode() {
-        // Logic to switch to dark mode
         System.out.println("Switched to Dark Mode");
         // Implement theme change logic here
     }
-
 }
