108199cfaef3bb96ddadd01e111e78f06a11a03b
krispierre <pierkl@farmingdale.edu>
2024-11-10 18:36:29 -0500

    fix setting window

--- src/main/java/mike/personalfitnesstracker/SettingsController.java
+++ src/main/java/mike/personalfitnesstracker/SettingsController.java
@@ -1,60 +1,61 @@
 package mike.personalfitnesstracker;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
 
 public class SettingsController {
 
-    @FXML
-    private ToggleGroup themeToggleGroup;
-    @FXML
-    private Button returnButton;
-    @FXML
-    private ComboBox<String> fontSizeComboBox;
+  @FXML
+    private Pane rootPane;
+
     @FXML
     private RadioButton lightModeRadioButton;
+
     @FXML
     private RadioButton darkModeRadioButton;
 
-    @FXML
+
     public void initialize() {
-
-        fontSizeComboBox.setItems(FXCollections.observableArrayList("Small", "Medium", "Large"));
+        setLightMode();
     }
+
 
     @FXML
-    private void handleReturnAction() {
-        System.out.println("Returning to the previous screen...");
-
+    private void handleThemeChange() {
+        if (lightModeRadioButton.isSelected()) {
+            setLightMode(); // Apply light theme if selected
+        } else if (darkModeRadioButton.isSelected()) {
+            setDarkMode();
+        }
     }
+
+
+    private void setLightMode() {
+        rootPane.getScene().getStylesheets().clear();
+        rootPane.getScene().getStylesheets().add(getClass().getResource("/styles/lightMode.css").toExternalForm());
+    }
+
+
+    private void setDarkMode() {
+        rootPane.getScene().getStylesheets().clear();
+        rootPane.getScene().getStylesheets().add(getClass().getResource("/styles/darkMode.css").toExternalForm());
+    }
+
 
     @FXML
     private void handleUpdateProfilePicture() {
-        System.out.println("Updating profile picture...");
 
     }
 
     @FXML
     private void handleChangeUsername() {
-        System.out.println("Changing username...");
-
-    }
-
-
-    @FXML
-    private void handleLightMode() {
-        System.out.println("Switched to Light Mode");
 
     }
 
     @FXML
-    private void handleDarkMode() {
-        System.out.println("Switched to Dark Mode");
+    private void handleReturnAction() {
 
     }
-
 }
+
