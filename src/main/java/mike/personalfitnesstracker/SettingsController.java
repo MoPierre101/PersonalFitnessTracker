d68b0c3c9c845835488ed207cee53c666f005b2e
molew-creator <molew679@gmail.com>
2024-11-20 21:32:33 -0500

    -Created regular expression for password input
    -Started 'Check-in' GUI
    
    -Still working on how to represent our data within Firebase DB

--- src/main/java/mike/personalfitnesstracker/SettingsController.java
+++ src/main/java/mike/personalfitnesstracker/SettingsController.java
@@ -1,23 +1,35 @@
 package mike.personalfitnesstracker;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SettingsController {
-
-    @FXML
-    private Pane rootPane;
 
     @FXML
     private RadioButton lightModeRadioButton;
 
     @FXML
     private RadioButton darkModeRadioButton;
+    @FXML
+    private ComboBox fontSizeComboBox;
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private ComboBox fontSizeComboBox1;
 
 
     public void initialize() {
-        setLightMode();
+
     }
 
 
@@ -34,14 +46,13 @@ public class SettingsController {
     private void setLightMode() {
 
         rootPane.getScene().getStylesheets().clear();
-
-        rootPane.getScene().getStylesheets().add(getClass().getResource("/styles/lightMode.css").toExternalForm());
+        rootPane.getScene().getStylesheets().add(getClass().getResource("resources/styles/lightMode.css").toExternalForm());
     }
 
 
     private void setDarkMode() {
         rootPane.getScene().getStylesheets().clear();
-        rootPane.getScene().getStylesheets().add(getClass().getResource("/styles/darkMode.css").toExternalForm());
+        rootPane.getScene().getStylesheets().add(getClass().getResource("resources/styles/darkMode.css").toExternalForm());
     }
 
 
@@ -56,7 +67,14 @@ public class SettingsController {
     }
 
     @FXML
-    private void handleReturnAction() {
+    private void handleReturnAction(ActionEvent actionEvent) throws IOException {
+        Parent homeParent = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Scene homeScene = new Scene(homeParent);
 
+        Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+
+        window.setScene(homeScene);
+        window.centerOnScreen();
+        window.show();
     }
 }
