14bb5f7c0ac5d12b46b22d66a065af3673aa7394
Ryan7714 <132383344+Ryan7714@users.noreply.github.com>
2024-12-07 21:23:19 -0500

    Added the profile picture code

--- src/main/java/mike/personalfitnesstracker/HomeController.java
+++ src/main/java/mike/personalfitnesstracker/HomeController.java
@@ -1,6 +1,11 @@
 package mike.personalfitnesstracker;
 
+import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.WriteResult;
 import com.google.cloud.storage.*;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -15,8 +20,10 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -43,6 +50,11 @@ public class HomeController {
     @FXML
     private ImageView avatorImageView;
 
+
+    @FXML
+    private Button chooseImageBtn;
+
+    private File pfpFile;
 
 
 
@@ -164,6 +176,111 @@ public class HomeController {
 
         System.out.println(profilePictureUrl);
         return profilePictureUrl;
+    }
+
+
+
+    private String uploadProfilePicture(File file){
+        String destinationPath = "pfps/OK.jpg";
+
+        if(file.exists()){
+
+
+            try {
+                // Initialize Firebase App
+                FileInputStream serviceAccount = new FileInputStream("src/main/resources/mike/personalfitnesstracker/key.json");
+                StorageOptions options = StorageOptions.newBuilder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+
+                Storage storage = options.getService();
+
+                String bucketName = "personal-fitness-tracker-66576.firebasestorage.app"; // Replace with your bucket name
+                destinationPath = "pfps/" + UUID.randomUUID() +  file.getName(); // Path in the bucket'
+                String filename = file.getName();
+                String fileExtension = filename.substring(filename.lastIndexOf(".") + 1);
+                // Read the file and upload to the bucket
+                byte[] fileBytes = Files.readAllBytes(file.toPath());
+                BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, destinationPath)
+                        .setContentType("image/"+fileExtension) // Set the MIME type
+                        .build();
+                storage.create(blobInfo, fileBytes);
+
+                System.out.println("File uploaded successfully to: " + destinationPath);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+        return destinationPath;
+    }
+
+
+    @FXML
+    public void chooseFileButtonClicked(ActionEvent actionEvent) {
+        handleChooseFileButtonClicked();
+    }
+
+    private void handleChooseFileButtonClicked(){
+        try {
+            // Configure FileChooser
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Select an Image to Upload");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
+            );
+
+            // Show FileChooser and get the selected file
+            Stage currentStage = (Stage) chooseImageBtn.getScene().getWindow();
+            File selectedFile = fileChooser.showOpenDialog(currentStage);
+            if (selectedFile != null) {
+                pfpFile = selectedFile;
+                updatePfpDB(uploadProfilePicture(pfpFile));
+                loadProfilePicture();
+
+            } else {
+                System.out.println("No file selected.");
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    public void handleChangeUsername(ActionEvent actionEvent) {
+    }
+
+    private void updatePfpDB(String blobInfo){
+        //asynchronously retrieve all documents from collection 'Person' in Firebase where the Username is equal to
+        //the currently logged-in user
+        ApiFuture<QuerySnapshot> query = Main.fstore.collection("Person")
+                .whereEqualTo("Username", LoginController.currentAccount.getUsername()).get();
+
+        try{
+            QuerySnapshot querySnapshot = query.get();
+            for(DocumentSnapshot document : querySnapshot.getDocuments()){
+
+                //Reference the document
+                DocumentReference personDoc = document.getReference();
+
+                //change the current account's weight to what the user checked-in with
+                LoginController.currentAccount.setPfpBlobInfo(blobInfo);
+
+                //update the 'Current Weight' field in Firebase
+                ApiFuture<WriteResult> writeResult = personDoc.update("Profile Picture BlobInfo", blobInfo);
+                System.out.println(blobInfo);
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
 
